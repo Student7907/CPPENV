b27dd608a483a5501376b44ab769525afe398c38
Student7907 <dream117709@gmail.com>
2026-06-04 20:52:51 +0530

    task 1 changes

--- main.cpp
+++ main.cpp
@@ -1,1 +1,48 @@
+#include <iostream>
+
+ 
+
+class Student {
+
+private:
+
+    int studentID;
+
+    string name;
+
+ 
+
+public:
+
+    Student(int studentID, string name) {
+
+        studentID = studentID;
+
+        name = name;
+
+    }
+
+ 
+
+    void displayInfo() {
+
+        cout << "Student ID: " << studentID << endl;
+
+        cout << "Name: " << name << endl;
+
+    }
+
+};
+
+ 
+
+int main() {
+
+    Student s(101, "Shubham");
+
+    s.displayInfo();
+
+    return 0;
+
+}
 
